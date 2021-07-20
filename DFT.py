b1c6fded2cf9d905eba7078111b7055719ce6018
andreapondini <andrea.pondini@studio.unibo.it>
2021-07-20 16:49:14 +0200

    removed name mangling

--- DFT.py
+++ DFT.py
@@ -50,7 +50,7 @@ class He:
         self.V_N[0]=0 #otherwise it diverges at 0
 
 
-    def __compute_hartree_potential(self):
+    def compute_hartree_potential(self):
         #   Compute Hartree potential from solving the Poisson equation
         #   U_H''(r) = -rho(r) / r
         #   with the boundary conditions U_H(0) = 0, U_H(r_max) = NUCLEAR_CHARGE.
@@ -68,10 +68,10 @@ class He:
         #get the Hartree potential from U_H
         self.V_H[1:] = U_H[1:] / self.r[1:]
         self.V_H[0] = 0
-    def __compute_exchange_potential(self):
+    def compute_exchange_potential(self):
         self.V_X[1:] = -np.cbrt(3*self.rho[1:]/(4*pi**2*self.r[1:]**2)) #Slater Potential
         self.V_X[0] = 0 #otherwise it diverges at 0
-    def __compute_correlation_potential(self):
+    def compute_correlation_potential(self):
         #Compute the correlation potential according to Ceperly-Alder
         #parameterization for the spin unpolarized system.
         A , B, C, D, GAM, BETA1, BETA2 = 0.0311, -0.048, 0.002, -0.0116, -0.1423, 1.0529, 0.3334
@@ -83,14 +83,14 @@ class He:
                 elif rs < 1e10 : self.V_C[i] = GAM / (1 + BETA1*rs**0.5 + BETA2*rs) * (1+BETA1*7/6*rs**0.5+BETA2*4/3*rs) / (1+BETA1*rs**0.5+BETA2*rs)
                 else: self.V_C[i]=0
 
-    def __hse_normalize(self): #to normalize radial u wavefunction
+    def hse_normalize(self): #to normalize radial u wavefunction
         probability_density=self.u**2
         probability = np.trapz(probability_density,self.r) 
         #the probability of finding an electron has to be = 1
         probability_density = probability_density/probability 
         self.u = probability_density**0.5
 
-    def __hse_integrate(self, L, E_N):
+    def hse_integrate(self, L, E_N):
           step = (self.r[-1] - self.r[0]) / (SAMPLES-1)
           #setting boundary codition
           self.u[-1] = self.r[-1]*np.exp(-self.r[-1])
@@ -99,20 +99,20 @@ class He:
           for i in range(SAMPLES-2,0,-1):
               self.u[i-1] = 2*self.u[i] - self.u[i+1] + step**2*(-2*E_N + 2*self.V[i] + L*(L+1)/self.r[i]**2)*self.u[i]
     
-    def __hse_solve(self,N,L): #solve SE
+    def hse_solve(self,N,L): #solve SE
         E_N=0
         E_max =  0
         E_min = HSE_E_MIN
         while np.abs(E_max-E_min) > PREC_HSE:
             E_N = (E_min+E_max)/2  #bisection method
-            self.__hse_integrate(L,E_N) #solve SE
+            self.hse_integrate(L,E_N) #solve SE
             nodes = 0 #look for nodes
             for i in range(0,SAMPLES-1):
                 if self.u[i]*self.u[i+1] < 0: nodes+=1
             #continue search in the above or below half of energy range
             if (nodes > N-L-1): E_max = E_N
             else: E_min = E_N 
-        self.__hse_normalize() #normalize WF
+        self.hse_normalize() #normalize WF
         return E_N
         
 
@@ -125,12 +125,12 @@ class He:
         while(np.abs(last_total_energy-self.total_energy)>PREC_DFT):
             last_total_energy = self.total_energy
             #breakpoint()
-            self.__compute_hartree_potential()
-            self.__compute_exchange_potential()
-            self.__compute_correlation_potential()
+            self.compute_hartree_potential()
+            self.compute_exchange_potential()
+            self.compute_correlation_potential()
             self.V = self.V_N + self.V_H  + self.V_X + self.V_C 
             #L is always 0 because s orbital, N = 1
-            self.E_k = self.__hse_solve(1, 0) #N,L
+            self.E_k = self.hse_solve(1, 0) #N,L
             self.rho = 2*self.u**2 #computes density, 2e- in 1s
             #total energy of the 2 electrons + potential energy
             self.total_energy = 2*self.E_k - self.potential_energy(self.V_H)  - self.potential_energy(self.V_X)/2 - self.potential_energy(self.V_C)/2 
